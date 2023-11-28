b6e1f5c88cb6365ee5d61deb9e1949540b2ca3e6
Elias Willerup <superdominoguy@gmail.com>
2023-11-28 00:16:09 -0700

    makepopst

---
+++ _posts/makepost.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (
+    set month=%%a
+    set day=%%b
+    set year=%%c
+)
+
+for /f "tokens=1-3 delims=:." %%a in ('echo %time%') do (
+    set hours=%%a
+    set minutes=%%b
+    set seconds=%%c
+)
+
+set filename=%year%-%month%-%day%_%hours%-%minutes%-%seconds%.md
+
+type nul > "%filename%"
+
+endlocal
